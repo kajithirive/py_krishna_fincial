2f1a51966dd27d33452c46415348a53ccc179e91
kajithirive <58061701+kajithirive@users.noreply.github.com>
2020-03-12 11:45:30 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path="C:\\Users\\Dell\\ChromeWebdriver")
-
+webdriver driver = new chromedriver ();
 driver.get("http://localhost:5000/")
-
 driver.find_element_by_name("click here to submit loan application form").click();
 

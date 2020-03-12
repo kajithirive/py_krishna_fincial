fc3b28cddee3d582282a9adfe6a4e5df16a74520
kajithirive <58061701+kajithirive@users.noreply.github.com>
2020-03-12 11:27:04 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,9 +5,3 @@ driver.get("http://localhost:5000/")
 
 driver.find_element_by_name("click here to submit loan application form").click();
 
-element = driver.find_element_by_name(“Your name”).sendkeys(“kaji”);
-element = driver.find_element_by_name(“Your email”).sendkeys(“kajithiriveedhi00@gmail.com”);
-element = driver.find_element_by_name(“Your age”).sendkeys(“24”);
-
-element = driver.find_element_by_name("submit").click()
-
